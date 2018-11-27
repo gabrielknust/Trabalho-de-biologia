0c764143941eb77016f272a5e0d70cfd0020dd68
gabrielknust <gabrielsilvaknustws@hotmail.com>
2018-11-27 18:40:43 -0200

    Add files via upload

---
+++ questoes.js
@@ -0,0 +1,93 @@
+var questoes=[
+    
+        {
+                "id":"1",
+                "enunciado":"(PUCCAMP-SP) Considere as afirmações abaixo relativas a fatores de crescimento populacional.<br/>I. A competição intraespecífica interfere na densidade da população.<br/>II. A competição interespecífica não influi no crescimento das populações.<br/>III. Um dos fatores limitantes do crescimento populacional é a disponibilidade de alimentos, que diminui quando a densidade da população aumenta.<br/>IV. Fatores climáticos influem no crescimento da população independentemente de sua densidade.<br/>São verdadeiras apenas:",
+                "a":" I e II.",
+                "b":" I e IV.",
+                "c":" II e III.",
+                "d":" I, III e IV.",
+                "e":"II, III e IV.",
+                "correta":"d",
+                "justificativa": "A única afirmação negativa é a II, pois a competição por recursos sempre influencia no crescimento e tamanho de uma população."
+        
+        },
+
+        {
+                "id":"2",
+                "enunciado":"Quando temos organismos da mesma espécie que trabalham unidos para o bem do grupo, temos um tipo de relação intraespecífica harmônica. Os agrupamentos que se caracterizam por possuírem divisão de trabalho, sistema de classes e indivíduos que apresentam relativa independência e mobilidade recebem o nome de:",
+                "a":"Colônia.",
+                "b":"Sociedade.",
+                "c":" Mutualismo.",
+                "d":" Protocooperação.",
+                "e": "Competição.",
+                "correta":"b",
+                "justificativa": "Em uma sociedade, indivíduos da mesma espécie apresentam grau de cooperação, divisão de trabalho e diferenças entre classes. Um exemplo de sociedade são as abelhas, em que é possível observar uma rainha, as operárias e os zangões trabalhando juntos em prol dessa sociedade."
+        
+        },
+        {
+                "id":"3",
+                "enunciado":"Sabemos que o mutualismo ocorre quando seres de espécies diferentes mantêm relações em que ambos são beneficiados. Marque a alternativa que indica organismos que estabelecem uma interação mutualística.",
+                "a":" Fungos e algas.",
+                "b":" Tubarão e rêmoras.",
+                "c":" Piolho e ser humano.",
+                "d":" Bromélias e árvores.",
+                "e": "Leões e zebras.",
+                "correta":"a",
+                "justificativa": "Os fungos e algas estabelecem um mutualismo obrigatório, formando os chamados líquens. Enquanto a alga fornece alimento para o fungo, este fornece água e nutrientes para a alga."
+        
+        },{
+                "id":"4",
+                "enunciado":"A rêmora é um peixe que estabelece uma relação bastante íntima com o tubarão, fixando-se em seu corpo e alimentando-se dos restos de alimentos que não foram digeridos pelo temido peixe. Essa relação é chamada de:",
+                "a":" Inquilinismo.",
+                "b":" Competição.",
+                "c":" Predação.",
+                "d":" Mutualismo.",
+                "e": "Comensalismo.",
+                "correta":"e",
+                "justificativa": "Essa relação é o comensalismo, uma vez que apenas a rêmora é beneficiada, porém o tubarão não é prejudicado. Nesse tipo de relação, o chamado comensal está normalmente procurando alimento."
+        
+        },
+        {
+                "id":"5",
+                "enunciado":" (Cesgranrio- RJ) Se duas espécies diferentes ocuparem num mesmo ecossistema o mesmo nicho ecológico, é provável que:",
+                "a":" se estabeleça entre elas uma relação harmônica.",
+                "b":" se estabeleça uma competição interespecífica.",
+                "c":" se estabeleça uma competição intraespecífica.",
+                "d":" uma das espécies seja produtora e a outra, consumidora.",
+                "e": "uma das espécies ocupe um nível trófico elevado.",
+                "correta":"b",
+                "justificativa": "Como as espécies estão ocupando o mesmo nicho, elas terão que competir pelos mesmos recursos. Sendo assim, haverá uma competição interespecífica, ou seja, entre espécies diferentes."
+        
+        },
+        {
+                "id":"6",
+                "enunciado":" (Fatec/2018)  A análise dos padrões genéticos e ecológicos de diversas espécies foi fundamental para a emergência de um novo campo de pesquisa no final do século XX: a Sociobiologia, segundo a qual a predominância do trabalho coletivo sobre o individual é uma das condições para a existência de sociedades complexas, como as encontradas entre os Hymenoptera (formigas, abelhas), os Isoptera (cupins) e os Homo sapiens. Entretanto, algumas espécies perderam evolutivamente a capacidade de trabalhar coletivamente.A predominância do trabalho individual sobre o coletivo nas espécies que perderam a capacidade de trabalhar coletivamente é, necessariamente, explicada por alterações nas relações ecológicas do tipo:",
+                "a":" intraespecífica desarmônica.",
+                "b":" intraespecífica harmônica.",
+                "c":" interespecífica neutra.",
+                "d":" interespecífica harmônica.",
+                "e": "interespecífica desarmônica.",
+                "correta":"b",
+                "justificativa": "A capacidade de trabalhar coletivamente como ocorre nas formigas, abelhas e cupins caracteriza uma relação harmônica intraespecífica (entre indivíduos de uma mesma espécie)."
+        
+        },
+        {
+                "id":"7",
+                "enunciado":" (IFS/2014) Os seres vivos mantêm relações entre si e com o meio onde vive. A respeito das relações ecológicas foram feitas algumas afirmações. Assinale a alternativa INCORRETA: ",
+                "a":" O predatismo é uma relação desarmônica entre a presa e o predador, essa relação é importante para o controle populacional das presas;",
+                "b":" A lombriga é considerada um endoparasita enquanto que a pulga e o piolho são ectoparasitas, estes organismos buscam abrigo e alimento no hospedeiro;",
+                "c":" As rêmoras são peixes que se grudam na pele dos tubarões com intuito de aproveitar os restos de suas presas, caracterizando o comensalismo;",
+                "d":" Os liquens são formados pela associação de fungos e algas, numa troca de benefícios obrigatória para a sobrevivência de ambos;.",
+                "e": "Os cupins não conseguem digerir a celulose sem a ajuda dos protozoários. Nos humanos esse polissacarídeo é facilmente digerido uma vez que produzimos a celulase, uma enzima específica para digestão do carboidrato citado.",
+                "correta":"e",
+                "justificativa": "O ser humano não possui a enzima celulase para quebra da celulose. Logo, esse polissacarídeo não é digerido pelo ser humano."
+        
+        }
+
+
+
+
+
+    
+];
