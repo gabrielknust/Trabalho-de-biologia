9390bac595fe197eb79cf51db3caadf49019bf1a
gabrielknust <gabrielsilvaknustws@hotmail.com>
2018-11-27 23:19:12 -0200

    zz

--- questoes.js
+++ questoes.js
@@ -51,11 +51,11 @@ var questoes=[
         {
                 "id":"5",
                 "enunciado":" (Cesgranrio- RJ) Se duas espécies diferentes ocuparem num mesmo ecossistema o mesmo nicho ecológico, é provável que:",
-                "a":" se estabeleça entre elas uma relação harmônica",
-                "b":" se estabeleça uma competição interespecífica",
-                "c":" se estabeleça uma competição intraespecífica",
-                "d":" uma das espécies seja produtora e a outra, consumidora",
-                "e": " uma das espécies ocupe um nível trófico elevado",
+                "a":" Se estabeleça entre elas uma relação harmônica",
+                "b":" Se estabeleça uma competição interespecífica",
+                "c":" Se estabeleça uma competição intraespecífica",
+                "d":" Uma das espécies seja produtora e a outra, consumidora",
+                "e": " Uma das espécies ocupe um nível trófico elevado",
                 "correta":"b",
                 "justificativa": "Como as espécies estão ocupando o mesmo nicho, elas terão que competir pelos mesmos recursos. Sendo assim, haverá uma competição interespecífica, ou seja, entre espécies diferentes."
         
@@ -63,11 +63,11 @@ var questoes=[
         {
                 "id":"6",
                 "enunciado":" (Fatec/2018)  A análise dos padrões genéticos e ecológicos de diversas espécies foi fundamental para a emergência de um novo campo de pesquisa no final do século XX: a Sociobiologia, segundo a qual a predominância do trabalho coletivo sobre o individual é uma das condições para a existência de sociedades complexas, como as encontradas entre os Hymenoptera (formigas, abelhas), os Isoptera (cupins) e os Homo sapiens. Entretanto, algumas espécies perderam evolutivamente a capacidade de trabalhar coletivamente.A predominância do trabalho individual sobre o coletivo nas espécies que perderam a capacidade de trabalhar coletivamente é, necessariamente, explicada por alterações nas relações ecológicas do tipo:",
-                "a":" intraespecífica desarmônica",
-                "b":" intraespecífica harmônica",
-                "c":" interespecífica neutra",
-                "d":" interespecífica harmônica",
-                "e": " interespecífica desarmônica",
+                "a":" Intraespecífica desarmônica",
+                "b":" Intraespecífica harmônica",
+                "c":" Interespecífica neutra",
+                "d":" Interespecífica harmônica",
+                "e": " Interespecífica desarmônica",
                 "correta":"b",
                 "justificativa": "A capacidade de trabalhar coletivamente como ocorre nas formigas, abelhas e cupins caracteriza uma relação harmônica intraespecífica (entre indivíduos de uma mesma espécie)."
         
@@ -88,11 +88,11 @@ var questoes=[
         {
                 "id":"8",
                 "enunciado":"(Unesp-SP) Um gavião, que tem sob suas penas carrapatos e piolhos, traz preso em suas garras um rato, com pulgas em seus pêlos. Entre o rato e as pulgas, entre os carrapatos e os piolhos e entre o gavião e o rato existem relações interespecíficas denominadas, respectivamente:" ,
-                "a":" inquilismo, competição e predatismo",
-                "b":" predatismo, competição e parasitismo",
-                "c":" parasitismo, competição e predatismo",
-                "d":" parasitismo, inquilinismo e predatismo",
-                "e": " parasitismo, predatismo e competição",
+                "a":" Inquilismo, competição e predatismo",
+                "b":" Predatismo, competição e parasitismo",
+                "c":" Parasitismo, competição e predatismo",
+                "d":" Parasitismo, inquilinismo e predatismo",
+                "e": " Parasitismo, predatismo e competição",
                 "correta":"c",
                 "justificativa": "Entre o rato e a pulga é um parasitismo, pois as pulgas são as únicas beneficiadas nessa troca, entre os carrapatos e piolhos há uma competição pelo alimento e entre o gavião e o rato é um predatismo, pois o rato vai servir de alimento para o gavião."
         
@@ -101,11 +101,11 @@ var questoes=[
 {
                 "id":"9",
                 "enunciado":"(Carlos Chagas-SP) A associação existente entre os ruminantes e as bactérias que vivem em seu estômago é um caso de:" ,
-                "a":" mutualismo",
-                "b":" parasitismo",
-                "c":" comensalismo",
-                "d":" competição interespecífica",
-                "e": " competição intraespecífica",
+                "a":" Mutualismo",
+                "b":" Parasitismo",
+                "c":" Comensalismo",
+                "d":" Competição interespecífica",
+                "e": " Competição intraespecífica",
                 "correta":"a",
                 "justificativa": " A relação é de mutualismo, pois os organismos dependem um do outro para sobreviver e eles se ajudam para que isso ocorra"
         
@@ -114,11 +114,11 @@ var questoes=[
 {
                 "id":"10",
                 "enunciado":" (UEL-PR) Considere o seguinte relato:“O pássaro-palito penetra na boca aberta do crocodilo removendo os restos de alimento e parasitas encontrados entre seus dentes. Assim, o pássaro obtém o seu alimento e livra o crocodilo de seus parasitas”. Esse caso é um exemplo de:",
-                "a":" protocooperação",
-                "b":" comensalismo",
-                "c":" inquilinismo",
-                "d":" mutualismo",
-                "e": " predativismo",
+                "a":" Protocooperação",
+                "b":" Comensalismo",
+                "c":" Inquilinismo",
+                "d":" Mutualismo",
+                "e": " Predativismo",
                 "correta":"a",
                 "justificativa": " A resposta é protocooperação, pois os animais em questão não dependem um do outro para sobreviver, mas mesmo assim se ajudam. "
         
@@ -126,11 +126,11 @@ var questoes=[
 {
                 "id":"11",
                 "enunciado":" (UFJF-MG) - Os líquens são formados pela associação de algas e fungos. Essa associação é:",
-                "a":" o mutualismo",
-                "b":" o predatismo",
-                "c":" a sociedade ",
-                "d":" o comensalismo ",
-                "e": " a colônia ",
+                "a":" Mutualismo",
+                "b":" Predatismo",
+                "c":" Sociedade ",
+                "d":" Comensalismo ",
+                "e": " Colônia ",
                 "correta":"a",
                 "justificativa": " Os líquens são constituídos por algas e fungos que vivem em associação, sendo que as algas, por meio da fotossíntese, geram matéria orgânica que o fungo usa como alimento. Por outro lado, o fungo consegue reter umidade e nutrientes que a alga utiliza, além de conferir proteção à alga. Esse mutualismo permite que os líquens sobrevivam em um ambiente que não seria possível a alga e o fungo sobreviverem individualmente "
         
@@ -141,11 +141,11 @@ var questoes=[
                 "enunciado":" (UniFor-2000) - Considere a frase abaixo. ‘O fungo Penicillium notatum produz penicilina, que impede a multiplicação de certas bactérias.’ Ela exemplifica um caso de:"
 
 ,
-                "a":" predatismo. ",
-                "b":" competição",
-                "c":" mutualismo ",
-                "d":" amensalismo",
-                "e": " comensalismo ",
+                "a":" Predatismo. ",
+                "b":" Competição",
+                "c":" Mutualismo ",
+                "d":" Amensalismo",
+                "e": " Comensalismo ",
                 "correta":"d",
                 "justificativa": " O amensalismo é a relação ecológica entre duas espécies em que uma inibe ou impede o desenvolvimento da outra, então por isso a d é a certa "
         
@@ -156,11 +156,11 @@ var questoes=[
                 "enunciado":" ENEM 2011 - Os vaga-lumes machos e fêmeas emitem sinais luminosos para se atraírem para o acasalamento. O macho reconhece a fêmea de sua espécie e, atraído por ela, vai ao seu encontro. Porém, existe um tipo de vaga-lume, o Photuris, cuja fêmea engana e atrai os machos de outro tipo, o Photinus, fingindo ser desse gênero. Quando o macho Photinus se aproxima da fêmea Photuris, muito maior que ele, é atacado e devorado por ela. BERTOLDI, O.G. VASCONCELOS, J.R. Ciências & Sociedade: a aventura da vida, a aventura da tecnologia. São Paulo: Scipione, 2000 (adaptado). A relação descrita no texto, entre a fêmea do gênero Photuris e o macho do gênero Photinus, é um exemplo de:" 
 
 ,
-                "a":" comensalismo ",
-                "b":" inquilinismo ",
-                "c":" cooperação ",
-                "d":" predatismo",
-                "e": " mutualismo ",
+                "a":" Comensalismo ",
+                "b":" Inquilinismo ",
+                "c":" Cooperação ",
+                "d":" Predatismo",
+                "e": " Muutualismo ",
                 "correta":"d",
                 "justificativa": " Como a fêmea Photuris devora o macho de outro gênero (Photinus), a relação estabelecida é a de predatismo. "
         
