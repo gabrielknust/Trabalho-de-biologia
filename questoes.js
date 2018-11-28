a4bc70a3cdb3cd0c61bebd8d99f88f462a14d49f
gabrielknust <gabrielsilvaknustws@hotmail.com>
2018-11-27 23:17:28 -0200

    z

--- questoes.js
+++ questoes.js
@@ -7,7 +7,7 @@ var questoes=[
                 "b":" I e IV",
                 "c":" II e III",
                 "d":" I, III e IV",
-                "e":"II, III e IV",
+                "e":" II, III e IV",
                 "correta":"d",
                 "justificativa": "A única afirmação negativa é a II, pois a competição por recursos sempre influencia no crescimento e tamanho de uma população."
         
@@ -16,11 +16,11 @@ var questoes=[
         {
                 "id":"2",
                 "enunciado":"Quando temos organismos da mesma espécie que trabalham unidos para o bem do grupo, temos um tipo de relação intraespecífica harmônica. Os agrupamentos que se caracterizam por possuírem divisão de trabalho, sistema de classes e indivíduos que apresentam relativa independência e mobilidade recebem o nome de:",
-                "a":"Colônia",
-                "b":"Sociedade",
+                "a":" Colônia",
+                "b":" Sociedade",
                 "c":" Mutualismo",
                 "d":" Protocooperação",
-                "e": "Competição",
+                "e": " Competição",
                 "correta":"b",
                 "justificativa": "Em uma sociedade, indivíduos da mesma espécie apresentam grau de cooperação, divisão de trabalho e diferenças entre classes. Um exemplo de sociedade são as abelhas, em que é possível observar uma rainha, as operárias e os zangões trabalhando juntos em prol dessa sociedade."
         
@@ -32,7 +32,7 @@ var questoes=[
                 "b":" Tubarão e rêmoras",
                 "c":" Piolho e ser humano",
                 "d":" Bromélias e árvores",
-                "e": "Leões e zebras",
+                "e": " Leões e zebras",
                 "correta":"a",
                 "justificativa": "Os fungos e algas estabelecem um mutualismo obrigatório, formando os chamados líquens. Enquanto a alga fornece alimento para o fungo, este fornece água e nutrientes para a alga."
         
@@ -43,7 +43,7 @@ var questoes=[
                 "b":" Competição",
                 "c":" Predação",
                 "d":" Mutualismo",
-                "e": "Comensalismo",
+                "e": " Comensalismo",
                 "correta":"e",
                 "justificativa": "Essa relação é o comensalismo, uma vez que apenas a rêmora é beneficiada, porém o tubarão não é prejudicado. Nesse tipo de relação, o chamado comensal está normalmente procurando alimento."
         
@@ -55,7 +55,7 @@ var questoes=[
                 "b":" se estabeleça uma competição interespecífica",
                 "c":" se estabeleça uma competição intraespecífica",
                 "d":" uma das espécies seja produtora e a outra, consumidora",
-                "e": "uma das espécies ocupe um nível trófico elevado",
+                "e": " uma das espécies ocupe um nível trófico elevado",
                 "correta":"b",
                 "justificativa": "Como as espécies estão ocupando o mesmo nicho, elas terão que competir pelos mesmos recursos. Sendo assim, haverá uma competição interespecífica, ou seja, entre espécies diferentes."
         
@@ -67,7 +67,7 @@ var questoes=[
                 "b":" intraespecífica harmônica",
                 "c":" interespecífica neutra",
                 "d":" interespecífica harmônica",
-                "e": "interespecífica desarmônica",
+                "e": " interespecífica desarmônica",
                 "correta":"b",
                 "justificativa": "A capacidade de trabalhar coletivamente como ocorre nas formigas, abelhas e cupins caracteriza uma relação harmônica intraespecífica (entre indivíduos de uma mesma espécie)."
         
@@ -80,7 +80,7 @@ var questoes=[
                 "b":" A lombriga é considerada um endoparasita enquanto que a pulga e o piolho são ectoparasitas, estes organismos buscam abrigo e alimento no hospedeiro",
                 "c":" As rêmoras são peixes que se grudam na pele dos tubarões com intuito de aproveitar os restos de suas presas, caracterizando o comensalismo",
                 "d":" Os liquens são formados pela associação de fungos e algas, numa troca de benefícios obrigatória para a sobrevivência de ambos",
-                "e": "Os cupins não conseguem digerir a celulose sem a ajuda dos protozoários. Nos humanos esse polissacarídeo é facilmente digerido uma vez que produzimos a celulase, uma enzima específica para digestão do carboidrato citado",
+                "e": " Os cupins não conseguem digerir a celulose sem a ajuda dos protozoários. Nos humanos esse polissacarídeo é facilmente digerido uma vez que produzimos a celulase, uma enzima específica para digestão do carboidrato citado",
                 "correta":"e",
                 "justificativa": "O ser humano não possui a enzima celulase para quebra da celulose. Logo, esse polissacarídeo não é digerido pelo ser humano." 
         
@@ -92,7 +92,7 @@ var questoes=[
                 "b":" predatismo, competição e parasitismo",
                 "c":" parasitismo, competição e predatismo",
                 "d":" parasitismo, inquilinismo e predatismo",
-                "e": "parasitismo, predatismo e competição",
+                "e": " parasitismo, predatismo e competição",
                 "correta":"c",
                 "justificativa": "Entre o rato e a pulga é um parasitismo, pois as pulgas são as únicas beneficiadas nessa troca, entre os carrapatos e piolhos há uma competição pelo alimento e entre o gavião e o rato é um predatismo, pois o rato vai servir de alimento para o gavião."
         
@@ -105,7 +105,7 @@ var questoes=[
                 "b":" parasitismo",
                 "c":" comensalismo",
                 "d":" competição interespecífica",
-                "e": "competição intraespecífica",
+                "e": " competição intraespecífica",
                 "correta":"a",
                 "justificativa": " A relação é de mutualismo, pois os organismos dependem um do outro para sobreviver e eles se ajudam para que isso ocorra"
         
@@ -118,7 +118,7 @@ var questoes=[
                 "b":" comensalismo",
                 "c":" inquilinismo",
                 "d":" mutualismo",
-                "e": "predativismo",
+                "e": " predativismo",
                 "correta":"a",
                 "justificativa": " A resposta é protocooperação, pois os animais em questão não dependem um do outro para sobreviver, mas mesmo assim se ajudam. "
         
@@ -130,7 +130,7 @@ var questoes=[
                 "b":" o predatismo",
                 "c":" a sociedade ",
                 "d":" o comensalismo ",
-                "e": "a colônia ",
+                "e": " a colônia ",
                 "correta":"a",
                 "justificativa": " Os líquens são constituídos por algas e fungos que vivem em associação, sendo que as algas, por meio da fotossíntese, geram matéria orgânica que o fungo usa como alimento. Por outro lado, o fungo consegue reter umidade e nutrientes que a alga utiliza, além de conferir proteção à alga. Esse mutualismo permite que os líquens sobrevivam em um ambiente que não seria possível a alga e o fungo sobreviverem individualmente "
         
@@ -142,10 +142,10 @@ var questoes=[
 
 ,
                 "a":" predatismo. ",
-                "b":"competição",
+                "b":" competição",
                 "c":" mutualismo ",
                 "d":" amensalismo",
-                "e": "comensalismo ",
+                "e": " comensalismo ",
                 "correta":"d",
                 "justificativa": " O amensalismo é a relação ecológica entre duas espécies em que uma inibe ou impede o desenvolvimento da outra, então por isso a d é a certa "
         
@@ -157,10 +157,10 @@ var questoes=[
 
 ,
                 "a":" comensalismo ",
-                "b":"inquilinismo ",
+                "b":" inquilinismo ",
                 "c":" cooperação ",
                 "d":" predatismo",
-                "e": "mutualismo ",
+                "e": " mutualismo ",
                 "correta":"d",
                 "justificativa": " Como a fêmea Photuris devora o macho de outro gênero (Photinus), a relação estabelecida é a de predatismo. "
         
